92661903de433a76e9c0427f30e01bf4c9dd7670
Groovman <37013381+Groovman@users.noreply.github.com>
2019-12-22 12:44:16 +0100

    add missing total for charaDamageUP

--- src/global_logic.js
+++ src/global_logic.js
@@ -2860,6 +2860,7 @@ module.exports.initializeTotals = function (totals) {
         totals[key]["debuffResistance"] = 0;
         totals[key]["cosmosDebuffResistance"] = 0;
         totals[key]["tenshiDamageUP"] = 0;
+        totals[key]["charaDamageUP"] = 0;
         totals[key]["charaUniqueDamageUP"] = 0;
         totals[key]["damageUPOnlyNormalBuff"] = 0;
         totals[key]['covenant'] = null;
